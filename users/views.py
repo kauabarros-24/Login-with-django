78a95b1526e9b27b4c0799347e49758052911717
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-05-10 15:18:26 -0300

    chore: registro de usuário realizado

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import UserSerializer
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
