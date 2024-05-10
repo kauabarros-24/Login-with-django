4bc65def2b0a8d3dcb584f0695e9a7c08f8eeb1d
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-05-10 15:39:15 -0300

    chore: login de usuário realizado

--- users/views.py
+++ users/views.py
@@ -1,9 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from .serializer import UserSerializer
+from .models import User
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        
+        user = User.objects.filter(email=email).first()
+        
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+        elif not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        return Response({
+            'message': 'Login realizado'
+        })
