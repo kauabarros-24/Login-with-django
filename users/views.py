a34a52a863b311d4dae6c6da7a4129c03916e1b0
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-05-10 15:51:30 -0300

    fix: arrumar erro de response

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializer import UserSerializer
 from .models import User
+import jwt, datetime
+from django.utils import timezone
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -12,16 +14,29 @@ class RegisterView(APIView):
     
 class LoginView(APIView):
     def post(self, request):
+        #Buscar usuário
         email = request.data['email']
         password = request.data['password']
         
         user = User.objects.filter(email=email).first()
         
+        #Validação de login
         if user is None:
             raise AuthenticationFailed('User not found!')
         elif not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
+        
 
-        return Response({
-            'message': 'Login realizado'
+        payload = {
+            'id': user.id,
+            'exp': timezone.now() + datetime.timedelta(minutes=60),
+            'iat': timezone.now()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response  = Response({
+            'jwt': token
         })
+        
+        return response
